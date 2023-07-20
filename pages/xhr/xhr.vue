d3c34887b139bfb548c10ca21a4e958a7bbdf55c
kags <kags@kagsdeiMac.lan>
2023-07-20 18:01:27 +0800

    feat: update pages

--- pages/xhr/xhr.vue
+++ pages/xhr/xhr.vue
@@ -14,31 +14,18 @@
 		// 	'content-type': 'application/json',
 		// 	a: 1
 		// },
-		timeout: 0
+		timeout: 0,
+		before: (config) => {
+			console.log(config, 'config');
+			return config;
+		},
+		after: (response) => {
+			console.log(response, 'response');
+			return response;
+		}
 	})
 	
-	// 请求拦截
-	xhr.interceptors.request.use((config) => {
-		// config.headers = {
-		// 	...config.headers,
-		// 	b: 2
-		// }
-		//  请求前追加参数
-		config.params = {
-			c: 3
-		}
-		return config;
-	}, (error) => {
-		console.log(error);
-		return Promise.reject(error);
-	}, xhr);
-	
-	// 响应拦截
-	xhr.interceptors.response.use((response) => {
-		// 可以随意修改响应数据
-		response['code'] = 2;
-		return response;
-	});
+	console.log(xhr);
 	
 	xhr
 		.setConfig({
@@ -60,14 +47,38 @@
 			console.log(err);
 		})
 		
-	// xhr
-	// 	.get('/v1/home/siteinfo')
-	// 	.then(res => {
-	// 		console.log(res);
-	// 	})
-	// 	.catch(err => {
-	// 		console.log(err);
-	// 	})
+	xhr
+		.get('/v1/home/siteinfo')
+		.then(res => {
+			console.log(res);
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	
+	// // 请求拦截
+	// xhr.interceptors.request.use((config) => {
+	// 	// config.headers = {
+	// 	// 	...config.headers,
+	// 	// 	b: 2
+	// 	// }
+	// 	//  请求前追加参数
+	// 	config.params = {
+	// 		c: 3
+	// 	}
+	// 	return config;
+	// }, (error) => {
+	// 	console.log(error, 'request 拦截');
+	// 	return Promise.reject(error);
+	// }, xhr);
+	
+	// // 响应拦截
+	// xhr.interceptors.response.use((response) => {
+	// 	// 可以随意修改响应数据
+	// 	console.log('响应拦截33');
+	// 	response['code'] = 2;
+	// 	return response;
+	// });
 </script>
 
 <style>
