6b94477afadc960b53fe82ebc06ab1608a81dfc1
kags <kags@kagsdeiMac.local>
2023-07-21 17:51:18 +0800

    feat: test

--- pages/xhr/xhr.vue
+++ pages/xhr/xhr.vue
@@ -5,80 +5,8 @@
 </template>
 
 <script lang="ts" setup>
-	import { LwuXHR, RequestConfig } from 'lwu-xhr';
-	console.log(LwuXHR);
-	// 创建实例
-	const xhr = LwuXHR.create({
-		baseURL: 'https://api.kags.cn',
-		// headers: {
-		// 	'content-type': 'application/json',
-		// 	a: 1
-		// },
-		timeout: 0,
-		before: (config) => {
-			console.log(config, 'config');
-			return config;
-		},
-		after: (response) => {
-			console.log(response, 'response');
-			return response;
-		}
-	})
-	
-	console.log(xhr);
-	
-	xhr
-		.setConfig({
-			params: {
-				a: 1,
-				b: 2,
-				...xhr.getConfig().params
-			},
-			data: {
-				c: 1
-			}
-			// dataType: 'string'
-		} as RequestConfig)
-		.get('/v1/home/siteinfo')
-		.then(res => {
-			console.log(res);
-		})
-		.catch(err => {
-			console.log(err);
-		})
-		
-	xhr
-		.get('/v1/home/siteinfo')
-		.then(res => {
-			console.log(res);
-		})
-		.catch(err => {
-			console.log(err);
-		})
-	
-	// // 请求拦截
-	// xhr.interceptors.request.use((config) => {
-	// 	// config.headers = {
-	// 	// 	...config.headers,
-	// 	// 	b: 2
-	// 	// }
-	// 	//  请求前追加参数
-	// 	config.params = {
-	// 		c: 3
-	// 	}
-	// 	return config;
-	// }, (error) => {
-	// 	console.log(error, 'request 拦截');
-	// 	return Promise.reject(error);
-	// }, xhr);
-	
-	// // 响应拦截
-	// xhr.interceptors.response.use((response) => {
-	// 	// 可以随意修改响应数据
-	// 	console.log('响应拦截33');
-	// 	response['code'] = 2;
-	// 	return response;
-	// });
+	import { requestDecorator } from 'lwu-xhr';
+	console.log(requestDecorator);
 </script>
 
 <style>
